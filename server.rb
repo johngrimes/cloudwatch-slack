ce430174b79818ccabab1544e645efba4566db36
John Grimes <john@smallspark.com.au>
2015-05-02 22:09:12 +1000

    Initial commit

---
+++ server.rb
@@ -0,0 +1,32 @@
+require 'bundler/setup'
+require 'sinatra/base'
+require 'json'
+require 'slack-notifier'
+
+class CloudwatchSlack < Sinatra::Base
+  def slack
+    Slack::Notifier.new ENV['SLACK_WEBHOOK_URL'],
+      :channel => ENV['SLACK_CHANNEL'], :username => 'Amazon Web Services',
+      :icon_url => ENV['ICON_URL']
+  end
+
+  post '/notify' do
+    sns = JSON.parse(request.body.read)
+    if sns['Type'] == 'Notification'
+      cw = JSON.parse(sns['Message'])
+      url = "https://console.aws.amazon.com/cloudwatch/home?region=" +
+        "#{ENV['AWS_REGION']}#alarm:alarmFilter=ANY;name=#{cw['AlarmName']}"
+      attachments = [{
+        :fallback => sns['Subject'],
+        :title => sns['Subject'],
+        :title_link => url,
+        :text => cw['NewStateReason'],
+        :color => 'danger'
+      }]
+      slack.ping 'CloudWatch alert triggered', :attachments => attachments
+    elsif sns['Type'] == 'SubscriptionConfirmation'
+      url = URI.parse(sns['SubscribeURL'])
+      response = Net::HTTP.get_response(url)
+    end
+  end
+end
